e8adfe6372f78074da6d633d098f887149327065
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-10-19 20:16:32 +0200

    header props.border

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,7 +1,7 @@
 import './Header.css';
 import { useHistory } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     let history = useHistory();
     const redirectToGallerie = () => {
         history.push('/Gallerie');
@@ -9,21 +9,36 @@ const Header = () => {
     const redirectToAcceuil = () => {
         history.push('/');
     }
+    const redirectToCatalogue = () => {
+        history.push('/Catalogue');
+    }
     return (
-        <div className='Header'>
-            <div className='header-startup-name-container'>
-                <h1 className='startup-name' onClick={redirectToAcceuil}>BarakaStore</h1>
-                <p className='startup-slogant'>DITES-LE AVEC DES FLEURS</p>
+        <>
+            <div className='Header'>
+                <div className='header-startup-name-container'>
+                    <h1 className='startup-name' onClick={redirectToAcceuil}>BarakaStore</h1>
+                    <p className='startup-slogant'>DITES-LE AVEC DES FLEURS</p>
+                </div>
+                <div className='header-option-container'>
+                    <ul className='option-ul header-ul'>
+                        <li className='option-li header-li' onClick={redirectToAcceuil} style={{
+                            borderBottom: props.acceuilBorder
+                        }}>Acceuil</li>
+                        <li className='option-li option-gallerie header-li' onClick={redirectToGallerie} style={{
+                            borderBottom: props.gallerieBorder
+                        }}>Gallerie</li>
+                        <li className='option-li option-catalogue header-li' onClick={redirectToCatalogue} style={{
+                            borderBottom: props.catalogueBorder
+                        }}>Catalogue</li>
+                        <li className='option-li option-login header-li' onClick={() => {
+                            history.push('/Souscrire')
+                        }} style={{
+                            borderBottom: props.souscrireBorder
+                        }}>Souscrire</li>
+                    </ul>
+                </div>
             </div>
-            <div className='header-option-container'>
-                <ul className='option-ul header-ul'>
-                    <li className='option-li option-acceuil header-li' onClick={redirectToAcceuil}>Acceuil</li>
-                    <li className='option-li option-gallerie header-li' onClick={redirectToGallerie}>Gallerie</li>
-                    <li className='option-li option-catalogue header-li'>Catalogue</li>
-                    <li className='option-li option-login header-li'>Souscrire</li>
-                </ul>
-            </div>
-        </div>
+        </>
     );
 }
 
