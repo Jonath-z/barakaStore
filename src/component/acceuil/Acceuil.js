ee4e5921612b69e0f707b0858e618ae585bb7094
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-10-23 18:03:06 +0200

    input phone amelioreted, send mai to client when he pass the command setted

--- src/component/acceuil/Acceuil.js
+++ src/component/acceuil/Acceuil.js
@@ -10,6 +10,12 @@ import { storageDB, realTimeDB } from '../modules/firebase';
 import uuid from 'react-uuid';
 import MediaQuery from 'react-responsive';
 import emailjs from 'emailjs-com';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
+import Datetime from 'react-datetime';
+import "react-datetime/css/react-datetime.css";
+import TextareaAutosize from "react-textarea-autosize";
+
 
 
 // ///////////////////////////////// ACCEUIL ///////////////////////////////////
@@ -65,17 +71,18 @@ export const Main = () => {
 }
 
 /////////////////////////////  ACCEUIL INPUT FORM ////////////////////////////////////////
-export const Acceuil = () => {
-    const [uploadedImg, setUploadedImg] = useState();
+export const Acceuil = (props) => {
+    const [uploadedImg, setUploadedImg] = useState('');
     const [clientEmail, setClientEmail] = useState('');
-    const [clientPhone, setClientPhone] = useState('');
+    const [clientPhone, setClientPhone] = useState();
     const [price, setPrice] = useState('');
     const [destinationEmail, setDestinationEmail] = useState('');
     const [destinationName, setDestinationName] = useState('');
-    const [destinationPhone, setDestinationPhone] = useState('');
+    const [destinationPhone, setDestinationPhone] = useState();
     const [commadDetails, setCommandDetails] = useState('');
     const [uploadState, setUploadState] = useState(0);
     const [options, setOptions] = useState();
+    const [date, setDate] = useState(new Date());
 
     useEffect(() => {
         realTimeDB.ref('/offers').on('value', (snapshot) => {
@@ -112,37 +119,48 @@ export const Acceuil = () => {
             destinationPhone: `${destinationPhone}`,
             commadDetails:`${commadDetails}`,
             payementProof: `${uploadedImg}`,
-            date: `${today.toLocaleString()}`
+            date: `${today.toLocaleString()}`,
+            livraryDate:`${date}`
         });
 // ************************** SEND EMAIL TO ADMIN *****************************************//
-        emailjs.send(`${process.env.REACT_APP_EMAILJS_SERVICE_ID}`, `${process.env.REACT_APP_EMAILJS_VALIDATE_TEMPLETE_ID}`, {
-            sujet: `Nouvelle Commande`,
+        emailjs.send(`${process.env.REACT_APP_EMAILJS_SERVICE_ID}`, `template_ux9bik9`, {
             to: `barakastore.drc@gmail.com`,
             reply_to: `barakastore.drc@gmail.com`,
-            name: `BarakaStore`,
+            client_email: `${clientEmail}`,
+            client_phone: `${clientPhone}`,
+            price: `${price}`,
+            delivrary_date: `${date}`,
+            date: `${today.toLocaleString()}`,
+            destination_name: `${destinationName}`,
+            destination_phone: `${destinationPhone}`,
+            command_details: `${commadDetails}`,
+            payement_proof:`${uploadedImg}`
+        }, `${process.env.REACT_APP_EMAILJS_USER_ID}`);
+// ************************** SEND EMAIL TO CLIENT *****************************************//
+        emailjs.send(`${process.env.REACT_APP_EMAILJS_SERVICE_ID}`, `${process.env.REACT_APP_EMAILJS_VALIDATE_TEMPLETE_ID}`, {
+            sujet: `Réception de la commande`,
+            to: `${clientEmail}`,
+            name: `${clientEmail.replace('@gmail.com','')}`,
+            reply_to: `barakastore.drc@gmail.com`,
             message: `
-                Vous avez une nouvelle commande,
-                Client: ${clientEmail},
-                phone: ${clientPhone},
-                prix: ${price}
-                Nom du recepteur : ${destinationName},
-                Numero du recepteur: ${destinationPhone},
-                Details de la commande : ${commadDetails}
-                Date de la commande: ${today.toLocaleString()}
-        `}, `${process.env.REACT_APP_EMAILJS_USER_ID}`);
+                Votre commande a été envoyée avec succès, nous reviendrons vers vous dans peu.
+            `
+        }, `${process.env.REACT_APP_EMAILJS_USER_ID}`);
         //*************************** CLEAR INPUTS ******************************************* */ 
         setClientEmail('');
-        setClientPhone('');
-        setPrice('');
+        setClientPhone();
+        // setPrice('');
         setDestinationEmail('');
         setDestinationName('');
-        setDestinationPhone('');
+        setDestinationPhone();
         setCommandDetails('');
         setUploadedImg('');
         setUploadState('0');
+        setDate(new Date());
         document.querySelector('.client-payement-proof').value = '';
         console.log(today.toLocaleString())
     }
+ 
 
     return (
         <>
@@ -151,7 +169,7 @@ export const Acceuil = () => {
                 <div className='acceuil-client-form'>
                     <h4 className='client-title'>Client</h4>
                     <input className='client-form-mail' type='email' placeholder='Email' value={clientEmail} name='clientMail' onChange={(e) => { setClientEmail(e.target.value) }} />
-                    <input type='phone' placeholder='phone' className='client-form-phone' value={clientPhone} name='clientPhone' onChange={(e) => { setClientPhone(e.target.value) }} />
+                    <PhoneInput international countryCallingCodeEditable={false} defaultCountry='CD' placeholder='phone' className='client-form-phone' value={clientPhone} name='clientPhone' onChange={setClientPhone} />
                     <h5 className='select-title'>Veuillez choisir le bouquet</h5>
                     <select className='main-form-select-price' onChange={(e) => { setPrice(e.target.value) }}>
                         <option value=''>choisir le bouquet</option>
@@ -161,20 +179,22 @@ export const Acceuil = () => {
                             })
                         }
                     </select>
+                    <h5 className='select-title'>Date et heure de livraison (48H minimum)</h5>
+                    <Datetime dateFormat={true} value={date} className='client-form-mail date-input' placeholder='Date' onChange={setDate} />
                 </div>
                 <div className='acceuil-client-form'>
                     <h4 className='client-title'>Destination</h4>
                     {/* <img src={uploadedImg} alt='uploaded'/> */}
                     <input className='client-form-mail' type='email' placeholder='Email (optionel)' value={destinationEmail} name='destinationMail' onChange={(e) => { setDestinationEmail(e.target.value) }} />
-                    <input className='client-form-name' type='text' placeholder='Name' value={destinationName} name='destinationName' onChange={(e) => { setDestinationName(e.target.value) }} />
-                    <input type='phone' placeholder='phone' className='client-form-phone' value={destinationPhone} name='destinationPhone' onChange={(e) => { setDestinationPhone(e.target.value) }} />
-                    <textarea placeholder='Details' className='client-text-textarea' value={commadDetails} name='commandDetails' onChange={(e) => { setCommandDetails(e.target.value) }} />
+                    <input className='client-form-name' type='text' placeholder='Nom' value={destinationName} name='destinationName' onChange={(e) => { setDestinationName(e.target.value) }} />
+                    <PhoneInput international countryCallingCodeEditable={false} defaultCountry='CD' placeholder='phone' className='client-form-phone' value={destinationPhone} name='destinationPhone' onChange={setDestinationPhone} />
+                    <TextareaAutosize placeholder='Couleur de roses,Adresse du destinateur et autre préférences' className='client-text-textarea' value={commadDetails} name='commandDetails' onChange={(e) => { setCommandDetails(e.target.value) }} />
                     <h5 className='select-title'>Preuve de paiyement     (<span>chargement: {uploadState}%</span>)</h5>
                     <input type='file' name='payementProof' className='client-payement-proof' onChange={uploadProof} />
                 </div>
                 <div className='submit-button-container'>
                     {
-                        uploadState === 100 && clientPhone.length >= 10 && clientEmail !== '' && destinationPhone.length >= 10 && commadDetails !== '' && price !== '' && <button className='submit-button' onClick={sendCommand}>ENVOYEZ</button>
+                        uploadState === 100 && uploadedImg !== '' && clientPhone.length >= 10 && clientEmail !== '' && destinationPhone.length >= 10 && commadDetails !== '' && price !== '' && <button className='submit-button' onClick={sendCommand}>ENVOYEZ</button>
                 
                     }
                 </div>
