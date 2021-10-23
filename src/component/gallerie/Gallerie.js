7b2c9c9ca553769f5d493311b75cafadbd27cff8
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-10-23 18:04:43 +0200

    photos snaphost cheked

--- src/component/gallerie/Gallerie.js
+++ src/component/gallerie/Gallerie.js
@@ -6,46 +6,42 @@ import { realTimeDB } from '../modules/firebase';
 import {useState,useEffect} from 'react';
 import uuid from 'react-uuid';
 
-const themeMemory = [];
-
-
-    const Gallerie = () => {
-        const [model, setmodel] = useState('');
-        const [displayForm, setDisplayForm] = useState('none');
-        const [galleryPhotos, setGalleryPhotos] = useState();
-        useEffect(() => {
-            realTimeDB.ref('/Gallerie').on('value', (snapshot) => {
+const Gallerie = () => {
+    const [model, setmodel] = useState('');
+    const [displayForm, setDisplayForm] = useState('none');
+    const [galleryPhotos, setGalleryPhotos] = useState();
+    const [themeMemory, setThemeMemory] = useState([]);
+    useEffect(() => {
+        realTimeDB.ref('/Gallerie').on('value', (snapshot) => {
+            if (snapshot.exists()) {
                 const photos = Object.values(snapshot.val());
-                // localStorage.setItem('gallerie', JSON.stringify(photos));
                 setGalleryPhotos(photos);
-                // setGalleryPhotos(photos);
                 photos.map(({ theme }) => {
                     if (themeMemory.indexOf(theme) === -1) {
+                        // themeMemory.push(theme);
+                        setThemeMemory([...themeMemory, theme]);
                         console.log(themeMemory);
-                        themeMemory.push(theme);
                     }
                     return (themeMemory);
                 })
                 console.log(photos);
-            });
-        }, [])
+            }
+        });
+    }, [themeMemory]);
 
-        const openForm = (e) => {
+    const openForm = (e) => {
             console.log(e.target.parentNode.firstChild.src);
             setmodel(e.target.parentNode.firstChild.src);
             setDisplayForm('block')
-            let x = window.screenX;
-            let y = window.screenY;
             window.scrollTo(0, 0);
-            window.onscroll = function () { window.scroll(x, y) };
         }
 
-        const GallerySorting = () => {
+    const GallerySorting = () => {
             return (
-                themeMemory.map(memoryTheme => {
+               themeMemory.length !== 0 && themeMemory.map(memoryTheme => {
                     return (
                         <>
-                            <div className='gallery-theme-container' key={uuid}>
+                            <div className='gallery-theme-container' key={uuid()}>
                                 <h4 className='gallery-theme' key={uuid()}>{memoryTheme}</h4>
                             </div>
                             <div className='gallery-photo-container' key={uuid()}>
@@ -53,8 +49,8 @@ const themeMemory = [];
                                     galleryPhotos !== undefined && galleryPhotos.reverse().map(({ theme, image }) => {
                                         return (
                                         theme === memoryTheme &&
-                                                <div className='image-global-div' onClick={openForm}>
-                                                    <img alt={theme} src={image} key={uuid()} className='gallery-photo' />
+                                                <div className='image-global-div' key={uuid()} onClick={openForm}>
+                                                    <img alt={theme} src={image} className='gallery-photo' />
                                                     <div className='image-details-container'>
                                                         <h4 className='gallery-theme theme-h4'>{theme}</h4><hr />
                                                         <p className='image-details'>Commadez comme model</p>
@@ -63,7 +59,7 @@ const themeMemory = [];
                                         )
                                     })
                                 }
-                                {galleryPhotos === undefined && <div className='image-global-div'>
+                                {galleryPhotos === undefined && <div className='image-global-div' key={uuid()}>
                                     <div className='image-details-container'>
                                         <GallerieSkeleton countValue={5} />
                                     </div>
@@ -79,35 +75,33 @@ const themeMemory = [];
             )
         }
 
-        return (
-            <>
-                <div className='gallerie'>
-                    <Header
-                        acceuilBorder={'none'}
-                        gallerieBorder={'2px solid rgb(68, 2, 2)'}
-                        catalogueBorder={'none'}
-                        souscrireBorder={'none'}
-                    />
-                    <div className='gallerie-content'>
-                        {
-                         <GallerySorting />
-                        }
-                    </div>
-                </div>
-                <GallerieCommandForm
-                    display={displayForm}
-                    model={model}
-                    closeCommande={
-                        () => {
-                            setDisplayForm('none')
-                            window.onscroll = function () { };
-                        }
-                    }
+    return (
+        <>
+            <div className='gallerie'>
+                <Header
+                    acceuilBorder={'none'}
+                    gallerieBorder={'2px solid rgb(68, 2, 2)'}
+                    catalogueBorder={'none'}
+                    souscrireBorder={'none'}
                 />
-            </>
+                <div className='gallerie-content'>
+                    {
+                        <GallerySorting />
+                    }
+                </div>
+            </div>
+            <GallerieCommandForm
+                display={displayForm}
+                model={model}
+                closeCommande={
+                    () => {
+                        setDisplayForm('none')
+                    }
+                }
+            />
+        </>
         
-        );
-    
+    );
 }
 
 export default Gallerie
